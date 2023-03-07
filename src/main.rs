f6853eb4b431be325984919f3c07ce8d60b2633c
Deefoozy <tinotrok@outlook.com>
2023-03-07 22:33:12 +0100

    Updated connection string to include dbname

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,13 @@ async fn main() -> std::io::Result<()> {
     check_env_key("DB_PASSWORD");
     check_env_key("DB_HOST");
 
+    if check_env_key("RUN_MIGRATIONS") == "yes" {
+        println!("Running migrations.");
+        db_migrate().await.expect("Migrations failed.");
+    } else {
+        println!("Skipping migrations.");
+    };
+
     println!("Starting server");
 
     HttpServer::new(|| {
@@ -30,9 +37,9 @@ async fn main() -> std::io::Result<()> {
                     .service(post_counter)
             )
     })
-    .bind(format!("0.0.0.0:{}", check_env_key("APP_PORT")) )?
-    .run()
-    .await
+        .bind(format!("0.0.0.0:{}", check_env_key("APP_PORT")))?
+        .run()
+        .await
 }
 
 #[get("/")]
@@ -70,9 +77,9 @@ async fn post_counter(req_body: String, counter_id: String) -> impl Responder {
 }
 
 async fn db_test() -> Result<(), Error> {
-    let user:String = check_env_key("DB_USER");
-    let password:String = check_env_key("DB_PASSWORD");
-    let host:String = check_env_key("DB_HOST");
+    let user: String = check_env_key("DB_USER");
+    let password: String = check_env_key("DB_PASSWORD");
+    let host: String = check_env_key("DB_HOST");
 
     // Connect to the database.
     let (client, connection) =
@@ -87,6 +94,45 @@ async fn db_test() -> Result<(), Error> {
     let _rows = client
         .execute("CREATE DATABASE test", &[])
         .await?;
+
+    Ok(())
+}
+
+mod embedded {
+    use refinery::embed_migrations;
+    embed_migrations!("./migrations");
+}
+
+async fn db_migrate() -> Result<(), Error> {
+    let user: String = check_env_key("DB_USER");
+    let password: String = check_env_key("DB_PASSWORD");
+    let host: String = check_env_key("DB_HOST");
+    let database: String = check_env_key("DB_NAME");
+
+    // Connect to the database.
+    let (mut client, connection) =
+        tokio_postgres::connect(
+            format!("host={} user={} password={} dbname={}", host, user, password, database).as_str(),
+            NoTls,
+        )
+            .await
+            .unwrap();
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    let migration_report = embedded::migrations::runner().run_async(&mut client).await.unwrap();
+
+    for migration in migration_report.applied_migrations() {
+        println!(
+            "Migration Applied -  Name: {}, Version: {}",
+            migration.name(),
+            migration.version()
+        );
+    }
 
     Ok(())
 }
