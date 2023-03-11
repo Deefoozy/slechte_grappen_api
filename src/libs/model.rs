e1ee499e96df6149df85bbe04ee871579431c146
Deefoozy <tinotrok@outlook.com>
2023-03-11 18:31:18 +0100

    updated way of converting String to str

--- src/libs/model.rs
+++ src/libs/model.rs
@@ -6,7 +6,7 @@ pub struct Model {}
 impl Model {
     pub async fn get_by_id(db_conn: &DatabaseConnection, table_name: String, id: &i64) -> Row {
         db_conn.client.query_one(
-            format!("SELECT * FROM {} WHERE id = $1", &table_name).as_str(),
+            &format!("SELECT * FROM {} WHERE id = $1", &table_name),
             &[&id]
         )
             .await
